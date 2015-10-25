70c3ddce5b907ed5feca3d04485a587c21b681fb
Sawood Alam <ibnesayeed@gmail.com>
2015-10-25 12:13:04 -0400

    Adding a cross platform build script, closes #27

---
+++ crossbuild.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+echo "Building binaries for various platforms"
+
+# Create directory to store all binaries
+binsdir=/tmp/mgbins
+mkdir -p $binsdir
+
+# Build binaries for Mac and Linux
+for GOOS in darwin linux; do
+  for GOARCH in 386 amd64; do
+    export GOOS=$GOOS
+    export GOARCH=$GOARCH
+    go build -v -o $binsdir/memgator-$GOOS-$GOARCH
+  done
+done
+
+# Build binaries for Windows
+for GOOS in windows; do
+  for GOARCH in 386 amd64; do
+    export GOOS=$GOOS
+    export GOARCH=$GOARCH
+    go build -v -o $binsdir/memgator-$GOOS-$GOARCH.exe
+  done
+done
+
+echo "Following binaries have been created in $binsdir"
+ls -l $binsdir
+
+exit 0
